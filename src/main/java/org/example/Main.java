382a9ed4abad94ec31d1063285cfe45bc939ae72
MrBl4ckRabbit <mr.bl4ck.rabbit@gmail.com>
2024-06-20 00:03:16 +0300

    Full project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String URL = "https://api.nasa.gov/planetary/apod?api_key=IwBTrFvtgYLw4jQz9yYTCxGfxo3LeVVTtL30UtF7&date=2024-06-19";
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(URL);
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+//        Scanner scanner = new Scanner(response.getEntity().getContent());
+//        if (scanner.hasNextLine()) {
+//            System.out.println(scanner.nextLine());
+//        } else {
+//            System.out.println("Ответ не содержит строк!");
+//        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+//        int statusCode = response.getCode();
+//        if (statusCode == 200) {
+//            // Обработка успешного ответа
+//            Scanner scanner = new Scanner(response.getEntity().getContent());
+//            if (scanner.hasNextLine()) {
+//                System.out.println(scanner.nextLine());
+//            } else {
+//                System.out.println("Ответ не содержит строк!");
+//            }
+//        } else {
+//            // Обработка ошибки
+//            System.out.println("Ошибка запроса: " + statusCode);
+//        }
+        ObjectMapper mapper = new ObjectMapper();
+        NASAAnswer answer = mapper.readValue(response.getEntity().getContent(), NASAAnswer.class);
+
+        String imgURL = answer.url;
+        String[] splittedURL = imgURL.split("/");
+        String fileName = splittedURL[splittedURL.length - 1];
+
+        HttpGet imageGet = new HttpGet(imgURL);
+        CloseableHttpResponse image = httpclient.execute(imageGet);
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        image.getEntity().writeTo(fileOutputStream);
+
     }
 }
